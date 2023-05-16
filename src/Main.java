d71efc99a8ad035e67705edfaa10c2319014e0fb
Anastaia <kvesteriko88@yandex.ru>
2023-05-16 11:19:59 +0300

    added calc bonus

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int miles = 20;
+        int bonus = price / miles;
+        int result = bonus;
+        System.out.println(result);
+    }
+}
